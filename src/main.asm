6762e01b85016ee3c99ffadba47ef626d1aae787
StefanosBekiaris <stefanos.bekiaris@maersk.com>
2023-08-15 12:47:54 +0100

    init

--- src/main.asm
+++ src/main.asm
@@ -1,66 +1,20 @@
 org 0x7C00
 bits 16
 
-
-%define ENDL 0x0D, 0x0A
-
-
-start:
-    jmp main
-
-
-;
-; Prints a string to the screen
-; Params:
-;   - ds:si points to string
-;
-puts:
-    ; save registers we will modify
-    push si
-    push ax
-    push bx
-
-.loop:
-    lodsb               ; loads next character in al
-    or al, al           ; verify if next character is null?
-    jz .done
-
-    mov ah, 0x0E        ; call bios interrupt
-    mov bh, 0           ; set page number to 0
-    int 0x10
-
-    jmp .loop
-
-.done:
-    pop bx
-    pop ax
-    pop si    
-    ret
-    
-
 main:
-    ; setup data segments
-    mov ax, 0           ; can't set ds/es directly
-    mov ds, ax
-    mov es, ax
-    
-    ; setup stack
-    mov ss, ax
-    mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
-
-    ; print hello world message
-    mov si, msg_hello
-    call puts
-
     hlt
 
 .halt
     jmp .halt
 
-
-
-msg_hello: db 'Hello world!', ENDL, 0
-
+; times = repeat instruction or directive
+;$ = obtain assembly position of the beginning of current line
+;$$ = obtain 
+; $-$$ gives current size of program
 
 times 510-($-$$) db 0
-dw 0AA55h
+
+
+; db = define bytes directive
+; dw = define words
+dw 0AA55h ; bios expects the last bit of a sector to be AA 55
