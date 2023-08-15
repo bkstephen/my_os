c7a6b42ea855020b03c2d6b985ad983d14156df4
StephenB <stefanosbek@hotmail.com>
2023-08-15 07:58:47 +0100

    Boot sequence

--- Boot/handler.h
+++
@@ -1,8 +0,0 @@
-#ifndef _HANDLER_H
-#define _HANDLER_H
-
-void init_timer(void);
-void enable_irq(void);
-void init_interrupt_controller(void);
-
-#endif
--- Boot/print.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PRINT_H
-#define _PRINT_H
-
-#include "stdint.h"
-
-int printk(const char *format, ...);
-
-#endif
